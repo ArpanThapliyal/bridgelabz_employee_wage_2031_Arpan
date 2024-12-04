6737e026357faacd55face72a07ecffbbae18548
unknown <arpan2031.be21@chitkara.edu.in>
2024-12-04 10:32:05 +0530

    use case1 code

---
+++ usecase1.js
@@ -0,0 +1,18 @@
+//check if present or not
+function attendance() {
+    const val = (Math.random()*10)+1;
+    if (val>6){
+        console.log("employe is present !!");
+    }
+    else{
+        console.log("employe is absent !!");
+    }
+}
+
+attendance();
+
+ 
+
+
+
+
